912683860577589047d50e144e01fd0dfe7e167f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-03-12 19:44:34 +0600

    updated example codes and documentations

--- src/main/java/dev/pages/ehsan/example/HelloApplication.java
+++ src/main/java/dev/pages/ehsan/example/HelloApplication.java
@@ -1,6 +1,7 @@
 package dev.pages.ehsan.example;
 
 import dev.pages.ehsan.utils.SceneManager;
+import dev.pages.ehsan.utils.Size;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,31 +9,51 @@ import java.io.IOException;
 
 
 public class HelloApplication extends Application {
-    public static SceneManager sm;
-
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-//        Original Codes to Open a FXML File
+        ////////////////////////////////////////
+        // Original Codes to Open a FXML File //
+        ////////////////////////////////////////
+
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 //        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 //        stage.setTitle("Hello!");
 //        stage.setScene(scene);
 //        stage.show();
 
-        // Scene Manager Codes for the same task
-        sm = new SceneManager(stage);
+        ///////////////////////////////////////////
+        // Scene Manager Codes for the same task //
+        ///////////////////////////////////////////
+
+        // Creating Scene Manager Objects
+        SceneManager sm = new SceneManager(stage);
+//        sm.open("home", "/dev/pages/ehsan/example/hello-view.fxml");
+        // Path must be "Path From Source Root"
+
+        // DO NOT CONFIGURE PAGE AFTER OPEN
+        // FOLLOW THE BELOW EXAMPLES IF YOU WANT TO CONFIGURE
+
+
+        ////////////////////////////////
+        // Add Custom Configurations //
+        ///////////////////////////////
+        // Add Default size for all pages
+        sm.setDefaultSize (new Size(400, 600));
+        // OR
+        sm.setDefaultSize (400, 600);
 
         // Add The Scene
         sm.add("home", "/dev/pages/ehsan/example/hello-view.fxml");
 
-        // Direct Open the Scene (DO NOT CONFIGURE PAGE AFTER OPEN)
-//        sm.open("home", "/dev/pages/ehsan/example/hello-view.fxml");
+        // Add/Open Scene with Custom Size
+//        sm.add("home", "/dev/pages/ehsan/example/hello-view.fxml", 400, 600);
+        // OR
+//        sm.add("home", "/dev/pages/ehsan/example/hello-view.fxml", new Size(400, 600));
 
-        // Configure
         // Change Window Title
         sm.setTitle("Test Application");
 
